4253f318e253da943e1c044a742e67793aeea1eb
Robin Schuster <r.schuster@computeruniverse.net>
2021-12-29 18:03:43 +0100

    Add stories with different image sizes

--- stories/Magnifier.stories.tsx
+++ stories/Magnifier.stories.tsx
@@ -7,15 +7,41 @@ export default {
     component: Magnifier,
 };
 
-const Template = () => (
-    <Magnifier
-        zoomImg={'https://picsum.photos/id/190/1000/1000'}
-        zoomImgHeight={1000}
-        zoomImgWidth={1000}
-        offsetLeft={10}
-    >
-        <img src={'https://picsum.photos/id/190/400/400'} height={400} width={400} alt="Lorem picsum" />
-    </Magnifier>
-);
+export const Basic = () => {
+    return (
+        <Magnifier
+            zoomImg={'https://picsum.photos/id/190/1000/1000'}
+            zoomImgHeight={1000}
+            zoomImgWidth={1000}
+            offsetLeft={10}
+        >
+            <img src={'https://picsum.photos/id/190/400/400'} height={400} width={400} alt="Lorem picsum" />
+        </Magnifier>
+    );
+};
 
-export const Primary = Template.bind({});
+export const AsyncImageSizes = () => {
+    return (
+        <Magnifier
+            zoomImg={'https://picsum.photos/id/234/800/1234'}
+            zoomImgHeight={1234}
+            zoomImgWidth={800}
+            offsetLeft={10}
+        >
+            <img src={'https://picsum.photos/id/234/500/300'} height={300} width={500} alt="Lorem picsum" />
+        </Magnifier>
+    );
+};
+
+export const AsyncImages = () => {
+    return (
+        <Magnifier
+            zoomImg={'https://picsum.photos/id/555/1000/1000'}
+            zoomImgHeight={1000}
+            zoomImgWidth={1000}
+            offsetLeft={10}
+        >
+            <img src={'https://picsum.photos/id/111/500/300'} height={300} width={500} alt="Lorem picsum" />
+        </Magnifier>
+    );
+};
